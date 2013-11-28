36ac922b0d911a44f0dbffabc9b69cd65cee1ee0
gabriman <gabrybass@gmail.com>
2013-11-28 12:21:04 +0100

    ALPHA VERSION SEPTEMBER + License GNU/GPL

--- simpleControlApp.m
+++ simpleControlApp.m
@@ -1,3 +1,19 @@
+%
+%Copyright 2013 Gabriel Rodríguez Rodríguez.
+%
+%This program is free software: you can redistribute it and/or modify
+%it under the terms of the GNU General Public License as published by
+%the Free Software Foundation, either version 3 of the License, or
+%(at your option) any later version.
+%
+%This program is distributed in the hope that it will be useful,
+%but WITHOUT ANY WARRANTY; without even the implied warranty of
+%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%GNU General Public License for more details.
+%
+%You should have received a copy of the GNU General Public License
+%along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 function varargout = simpleControlApp(varargin)
 %SIMPLECONTROLAPP M-file for simpleControlApp.fig
 %      SIMPLECONTROLAPP, by itself, creates a new SIMPLECONTROLAPP or raises the existing
@@ -107,7 +123,7 @@ if handles.initialized == false
         return
     end
     
-    handles.imgLoading = imread('gif-loading.jpg');
+    %handles.imgLoading = imread('gif-loading.jpg');
     
     [code,message] = camControl_initCheck(handles.options);
     if strcmp(code,'0')
